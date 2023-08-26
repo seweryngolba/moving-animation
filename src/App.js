683472cf4d83e08d1f801fca1222fa11b8e5061a
Seweryn Golba <sgoolb@gmail.com>
2023-08-26 23:32:49 +0200

    floating element

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 // App.js
 
 import React from "react";
-import { ScrollContainer, ScrollPage } from "react-scroll-motion";
+import {
+  Animator,
+  Fade,
+  ScrollContainer,
+  ScrollPage,
+  batch,
+  Sticky,
+} from "react-scroll-motion";
 import Landing from "./Components/Landing/Landing";
 import TextArea from "./Components/TextArea/TextArea";
 import Contact from "./Components/Contact/Contact";
 import ScrollSectionContent from "./Components/ScrollSection/ScrollSection"; // Import nowego komponentu
+import Floating from "./Components/Floating/Floating";
 
 function App() {
   return (
@@ -18,6 +26,11 @@ function App() {
       </ScrollPage>
       <ScrollPage page={2}>
         <Contact />
+      </ScrollPage>
+      <ScrollPage page={3}>
+        <Animator animation={batch(Sticky(), Fade())}>
+          <Floating />
+        </Animator>
       </ScrollPage>
       <ScrollSectionContent />
     </ScrollContainer>
---
+++ src/Components/Floating/Floating.js
@@ -0,0 +1,11 @@
+import "./Floating.css";
+
+const Floating = () => {
+  return (
+    <div>
+      <div className="floatingAv"></div>
+    </div>
+  );
+};
+
+export default Floating;
